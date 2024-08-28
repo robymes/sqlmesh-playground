480eb4dfb7259e29fd5d74103f0dc99662fa5f38
Roberto Messora <roberto.messora@jakala.com>
2024-08-28 22:03:20 +0000

    environment config update

--- .devcontainer/init-postgres-multidb.sh
+++ .devcontainer/init-postgres-multidb.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_USER" -h "localhost" <<-EOSQL
     CREATE DATABASE sql_mesh_state;
     CREATE DATABASE sql_mesh_dwh;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "sql_mesh_state" <<-EOSQL
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "sql_mesh_state" -h "localhost" <<-EOSQL
     CREATE USER sql_mesh_state_user WITH PASSWORD 'sql_mesh_state_pwd';
     GRANT ALL PRIVILEGES ON DATABASE sql_mesh_state TO sql_mesh_state_user;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "sql_mesh_dwh" <<-EOSQL
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "sql_mesh_dwh" -h "localhost" <<-EOSQL
     CREATE USER sql_mesh_dwh_user WITH PASSWORD 'sql_mesh_dwh_pwd';
     GRANT ALL PRIVILEGES ON DATABASE sql_mesh_dwh TO sql_mesh_dwh_user;
 EOSQL
