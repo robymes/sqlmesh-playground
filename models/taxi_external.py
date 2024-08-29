8842d05e2e788dc3dfb0295c46ae5917600083ee
Roberto Messora <roberto.messora@jakala.com>
2024-08-29 14:38:10 +0000

    external model update

--- models/taxi_external.py
+++ models/taxi_external.py
@@ -32,6 +32,7 @@ from sqlmesh.core.model.kind import ModelKindName
         "airport_fee": "float"
     },
 )
+
 def execute(
     context: ExecutionContext,
     start: datetime,
@@ -40,23 +41,32 @@ def execute(
     **kwargs: t.Any,
 ) -> pd.DataFrame:
     
+    def fetch_data_in_chunks(connection, query, chunk_size=10000, max_offset=10):    
+        offset = 0
+        while True:
+            chunk_query = f"{query} LIMIT {chunk_size} OFFSET {offset}"
+            chunk = connection.execute(chunk_query).fetchdf()
+            if offset >= (max_offset * chunk_size):
+                break
+            yield chunk
+            offset += chunk_size
+
+    def process_data(chunk):
+        return chunk
+    
     conn = duckdb.connect('md:?motherduck_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJvYnltZXNAZ21haWwuY29tIiwic2Vzc2lvbiI6InJvYnltZXMuZ21haWwuY29tIiwicGF0IjoiLXlnd3RNTmc4T0dnMmVnb24xVXdzWUZVQjUzRWZHSVJpT21EMTRmTFE2VSIsInVzZXJJZCI6IjAyMDJjMzA2LTljNWMtNGMzYS1iZGVlLTdlNWFlZTZkY2M0ZSIsImlzcyI6Im1kX3BhdCIsImlhdCI6MTcyNDQwNzkxOH0.vNEM1o48K0R78zC7xeCrQuGcJpCoyB6UG-hJL7fb7VI')
     table_name = "sample_data.nyc.taxi"
-    count_query = f"SELECT COUNT(*) FROM {table_name}"
-    # query = f"SELECT * FROM {table_name}"
-    query = f"SELECT * FROM {table_name} LIMIT 100000"
-    total_rows = conn.execute(count_query).fetchone()[0]
-    chunk_size = 100000
-    dfs = []
+    
+    query = f"SELECT * FROM {table_name}"
+    chunk_size = 10000
+    max_offset = 10
+    results = []
 
-    # for offset in range(0, total_rows, chunk_size):
-    #     chunk_query = f"{query} LIMIT {chunk_size} OFFSET {offset}"
-    #     chunk_df = conn.execute(chunk_query).df()
-    #     dfs.append(chunk_df)
+    for chunk in fetch_data_in_chunks(conn, query, chunk_size, max_offset):
+        processed_chunk = process_data(chunk)
+        results.append(processed_chunk)
 
-    # df = pd.concat(dfs, ignore_index=True)
-
-    df = conn.execute(query).df()
+    df = pd.concat(results, ignore_index=True)
 
     conn.close()
 
