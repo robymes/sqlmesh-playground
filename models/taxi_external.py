70a6cc8cbe8ade9a30ecf464089af132ed30687f
Roberto Messora <roberto.messora@jakala.com>
2024-08-29 15:05:03 +0000

    python external model update

--- models/taxi_external.py
+++ models/taxi_external.py
@@ -41,33 +41,15 @@ def execute(
     **kwargs: t.Any,
 ) -> pd.DataFrame:
     
-    def fetch_data_in_chunks(connection, query, chunk_size=10000, max_offset=10):    
-        offset = 0
-        while True:
-            chunk_query = f"{query} LIMIT {chunk_size} OFFSET {offset}"
-            chunk = connection.execute(chunk_query).fetchdf()
-            if offset >= (max_offset * chunk_size):
-                break
-            yield chunk
-            offset += chunk_size
-
-    def process_data(chunk):
-        return chunk
-    
     conn = duckdb.connect('md:?motherduck_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJvYnltZXNAZ21haWwuY29tIiwic2Vzc2lvbiI6InJvYnltZXMuZ21haWwuY29tIiwicGF0IjoiLXlnd3RNTmc4T0dnMmVnb24xVXdzWUZVQjUzRWZHSVJpT21EMTRmTFE2VSIsInVzZXJJZCI6IjAyMDJjMzA2LTljNWMtNGMzYS1iZGVlLTdlNWFlZTZkY2M0ZSIsImlzcyI6Im1kX3BhdCIsImlhdCI6MTcyNDQwNzkxOH0.vNEM1o48K0R78zC7xeCrQuGcJpCoyB6UG-hJL7fb7VI')
     table_name = "sample_data.nyc.taxi"
-    
-    query = f"SELECT * FROM {table_name}"
+        
     chunk_size = 10000
-    max_offset = 10
-    results = []
+    iterations = 10
 
-    for chunk in fetch_data_in_chunks(conn, query, chunk_size, max_offset):
-        processed_chunk = process_data(chunk)
-        results.append(processed_chunk)
-
-    df = pd.concat(results, ignore_index=True)
+    for i in range(iterations):
+        query = f"SELECT * FROM {table_name} LIMIT {chunk_size} OFFSET {i}"
+        df = conn.execute(query).fetchdf()
+        yield df
 
     conn.close()
-
-    return df
